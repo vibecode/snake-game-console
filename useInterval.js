86a7585b9b6ceb8a3e58aaef6c90e695e64f5783
vibecode <rumashkin@gmail.com>
2019-09-18 20:31:37 +0200

    implement use interval

--- useInterval.js
+++ useInterval.js
@@ -1,3 +1,17 @@
 const { useEffect, useRef } = required('react')
 
-module.exports = function useInterval(cb, delay) {}
+module.exports = function useInterval(cb, delay) {
+	const savedCallback = useRef()
+
+	useEffect(() => {
+		savedCallback.current = cb
+	}, [callback])
+
+	//set up the interval
+	useEffect(() => {
+		if (delay !== null) {
+			let id = setInterval(() => savedCallback.current(), delay)
+			return () => clearInterval(id)
+		}
+	}, [delay])
+}
